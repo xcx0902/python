7f52458d861ca1630948ac9f111db359c1e5ba98
Xu Chenxuan <99001676+xcx0902@users.noreply.github.com>
2026-04-28 12:25:22 +0800

    Update luogu-auto-punch.py

--- luogu-auto-punch.py
+++ luogu-auto-punch.py
@@ -5,7 +5,7 @@ import json
 import sys
 
 def punch(cookie):
-    return requests.get('https://www.luogu.com.cn/index/ajax_punch', headers={
+    return requests.post('https://www.luogu.com.cn/index/ajax_punch', headers={
         "Host": "www.luogu.com.cn",
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv: 73.0) Gecko/20100101 Firefox/73.0",
         "Accept": "*/*",

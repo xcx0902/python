a7e069265431c623eeef2fecdff895876a085b3f
Xu Chenxuan <99001676+xcx0902@users.noreply.github.com>
2026-04-30 20:05:54 +0800

    Update luogu-auto-punch.py

--- luogu-auto-punch.py
+++ luogu-auto-punch.py
@@ -3,6 +3,28 @@
 import requests
 import json
 import sys
+from bs4 import BeautifulSoup
+
+def GetCSRF(cookie):
+    content = requests.get('https://www.luogu.com.cn', headers = {
+        'accept': '*/*',
+        'accept-language': 'en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7',
+        'content-length': '0',
+        'origin': 'https://www.luogu.com.cn',
+        'priority': 'u=1, i',
+        'referer': 'https://www.luogu.com.cn/',
+        'sec-ch-ua': '"Google Chrome";v="147", "Not.A/Brand";v="8", "Chromium";v="147"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"macOS"',
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'same-origin',
+        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/147.0.0.0 Safari/537.36',
+        "Cookie": cookie
+    }).text
+    soup = BeautifulSoup(content, 'html.parser')
+    token = soup.find('meta', {'name': 'csrf-token'})['content']
+    return token
 
 def punch(cookie):
     return requests.post('https://www.luogu.com.cn/index/ajax_punch', headers = {
@@ -19,7 +41,7 @@ def punch(cookie):
         'sec-fetch-mode': 'cors',
         'sec-fetch-site': 'same-origin',
         'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/147.0.0.0 Safari/537.36',
-        'x-csrf-token': '1777541137:QXL7WBvmc+RuDypGip7ZofXe58P82c6BEk6KqHZbciY=',
+        'x-csrf-token': GetCSRF(cookie),
         'x-requested-with': 'XMLHttpRequest',
         "Cookie": cookie
     }).text
